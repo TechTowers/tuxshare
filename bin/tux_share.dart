802b24f717c7742b3dd39bbab1f3855ecf6fba00
Karun Sandhu <129101708+MrSom3body@users.noreply.github.com>
2025-05-06 17:26:18 +0200

    treewide: rename tux_share to tuxshare

--- bin/tux_share.dart
+++
@@ -1,5 +0,0 @@
-import 'package:tux_share/tux_share.dart' as tux_share;
-
-void main(List<String> arguments) {
-  print('Hello world: ${tux_share.calculate()}!');
-}
--- test/tuxshare_test.dart
+++ test/tuxshare_test.dart
@@ -1,4 +1,4 @@
-import 'package:tux_share/tux_share.dart';
+import 'package:tuxshare/tuxshare.dart';
 import 'package:test/test.dart';
 
 void main() {
