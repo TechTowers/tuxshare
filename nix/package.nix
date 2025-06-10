7c3562d3160bfa4f1730f31b13189524da6fb6d7
Karun Sandhu <129101708+MrSom3body@users.noreply.github.com>
2025-06-10 12:50:28 +0200

    treewide: bump version

--- nix/package.nix
+++ nix/package.nix
@@ -1,7 +1,7 @@
 {buildDartApplication}:
 buildDartApplication rec {
   pname = "tuxshare";
-  version = "1.1.0";
+  version = "1.1.1";
   src = ../.;
 
   dartEntryPoints."bin/tuxshare" = "bin/tuxshare.dart";
